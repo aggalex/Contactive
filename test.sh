4d0baeae94010884f4d107d96f33298c0654be19
Alex Angelou <ubuntu1aggelalex@gmail.com>
2021-03-20 22:25:03 +0200

    Remove unnecessary files

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-URL="http://localhost:8000"
-
-# curl $URL/register -X POST -H "Content-Type: application/json" -d '{"username": "aggelalex", "email": "ubuntu1aggelalex@gmail.com", "password": "yomama"}'
-curl $URL/login -X POST -H "Content-Type: application/json" -d '{"username": "sjk", "password": "jljkl"}'
